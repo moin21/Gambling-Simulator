c126d5dd12e65934a8692e957fe5d47618bef12f
moin21 <mamoinuddin@gmail.com>
2022-02-09 12:16:04 +0530

    Added: Daily Stake and Per Game Bet

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,5 +1,7 @@
 public class GamblingSimulator {
         public static void main(String args[]){
             System.out.println("Welcome To Gambling Simulator");
+            int dailyStake = 100;
+            int perGameBet = 1;
         }
 }
